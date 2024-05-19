b9ac5c5fe7d3af76b9da700dd2e553b6ca684917
ccirelli2 <ccirelli2@gmail.com>
2024-05-19 11:07:51 -0400

    added poetry environment and updated read me file.

---
+++ examples/use_llama_w_hf_transformers.py
@@ -0,0 +1,32 @@
+"""
+Purpose:
+- Use meta llama3 model locally.
+
+References
+- model page: https://huggingface.co/meta-llama/Meta-Llama-3-8B
+- hf-pipelines:
+    - overview: https://huggingface.co/transformers/
+    - source: https://github.com/huggingface/transformers/blob/v4.41.0/src/transformers/pipelines/__init__.py#L562
+    - description: provides a simple API for NLP tasks like using models for inference.
+"""
+import os
+import torch
+import transformers
+from decouple import config as d_config
+
+# Configurations
+MODEL_ID = "meta-llama/Meta-Llama-3-8B"
+HF_TOKEN = d_config('HF_TOKEN')
+
+# Load Model
+pipeline = transformers.pipeline(
+    task="text-generation",
+    model=MODEL_ID,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+    token=HF_TOKEN
+)
+
+
+
+
